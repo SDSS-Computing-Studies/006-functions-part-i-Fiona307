6b0992815cd969c4b160c4429831a86237d11f30
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-10-07 12:35:03 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -6,3 +6,14 @@ The input is a list.
 The return value is the largest value in the list
 (2 points)
 """
+List = []
+def largest(List):
+    num = int(input("Enter the length of the list:"))
+    for i in range(0,num):
+        b = float(input("Enter the number in the list:"))
+        List.append(b)
+    List.sort()
+    answer = List[-1]
+    return answer
+x = largest(List)
+print(x)
