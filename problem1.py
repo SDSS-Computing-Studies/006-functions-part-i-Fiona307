da7e8c622c94ee2e23b843390f8a39de9bbc817d
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-10-07 15:18:21 -0700

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -7,3 +7,19 @@ If the boolean is False, then the larger number is the hypotenuse
 Return the missing side
 (2 points)
 """
+import math
+
+def hypotenuse(a,b,boolean):
+    if boolean == True:
+        answer = math.sqrt(float(a)**2 + float(b)**2)
+    if boolean == False:
+        if a > b:
+            answer = math.sqrt(float(a)**2 - float(b)**2)
+        elif a < b:
+            answer = math.sqrt(float(b)**2 - float(a)**2)
+    return answer
+
+x = hypotenuse(3,4,False)
+print(x)
+y = hypotenuse(13,5,True)
+print(y)
