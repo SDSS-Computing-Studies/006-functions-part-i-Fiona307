0eca618a20e3642390cb0bb647bc39eaa44fd747
Fiona307 <71406797+Fiona307@users.noreply.github.com>
2020-10-08 11:15:53 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -9,12 +9,12 @@ Note that the coordinates should be signed (positive or negative) floats
 """
 import math
 
-def distance(tup1,tup2,rnd):
+def distance(tup1,tup2):
 
-    answer = round(math.sqrt((float(tup1[0])-float(tup2[0]))**2 + (float(tup1[1])-float(tup2[1]))**2),rnd)
+    answer = round(math.sqrt((float(tup1[0])-float(tup2[0]))**2 + (float(tup1[1])-float(tup2[1]))**2),3)
     return answer
 
-x = distance((2,4),(6,3),3)
+x = distance((2,4),(6,3))
 print(x)
-y = distance((-3,2.2),(1,2),3)
+y = distance((-3,2.2),(1,2))
 print(y)
